d6e9bbab4fd9da2278e2e4763552a8539f19e9b9
swallowvski <ahoudori7779@icloud.com>
2022-01-18 09:38:47 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+#![allow(
+    dead_code,
+    unused_variables,
+    clippy::too_many_arguments,
+    clippy::unnecessary_wraps
+)]
+
+use std::collections::HashSet;
+use std::ffi::CStr;
+use std::os::raw::c_void;
+
+use log::*;
+
+use anyhow::{anyhow, Result};
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
+use vulkanalia::vk::ExtDebugUtilsExtension;
+
+const VALIDATION_ENABLED: bool = cfg!(debug_assertions);
+const VALIDATION_LAYER: vk::ExtensionName =
+    vk::ExtensionName::from_bytes(b"VK_LAYER_KHRONOS_validation");
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Vulkan Tutorial (Rust)")
+        .with_inner_size(LogicalSize::new(1024.0, 768.0))
+        .build(&event_loop)?;
+
+    let mut app = unsafe { App::create(&window)? };
+    let mut destroying = false;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+        match event {
+            Event::MainEventsCleared if !destroying => unsafe { app.render(&window) }.unwrap(),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                destroying = true;
+                *control_flow = ControlFlow::Exit;
+                unsafe {
+                    app.destroy();
+                }
+            }
+            _ => {}
+        }
+    });
+}
+
+#[derive(Clone, Debug)]
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
+
+impl App {
+    unsafe fn create(window: &Window) -> Result<Self> {
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{b}"))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
+    }
+
+    unsafe fn render(&mut self, window: &Window) -> Result<()> {
+        Ok(())
+    }
+
+    unsafe fn destroy(&mut self) {
+        self.instance.destroy_instance(None);
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+struct AppData {}
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial (Rust)\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let available_layer = entry
+        .enumerate_instance_layer_properties()?
+        .iter()
+        .map(|l| l.layer_name)
+        .collect::<HashSet<_>>();
+
+    if VALIDATION_ENABLED && !available_layer.contains(&VALIDATION_LAYER) {
+        return Err(anyhow!("Validation layre requested but not supported"));
+    }
+
+    let layers = if VALIDATION_ENABLED {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        Vec::new()
+    };
+
+    let extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions);
+
+    Ok(entry.create_instance(&info, None)?)
+}
