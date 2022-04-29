d178277281d05936341f135f05682e34d02eecfe
swallowvski <ahoudori7779@icloud.com>
2022-04-30 00:52:18 +0900

    base code

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,48 @@
-fn main() {}
+#![allow(
+    dead_code,
+    unused_variables,
+    clippy::too_many_arguments,
+    clippy::unnecessary_wraps
+)]
+
+use anyhow::Result;
+use winit::{
+    dpi::LogicalSize,
+    event::Event,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
+fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Vulkan Tutorial (Rust)")
+        .with_inner_size(LogicalSize::new(1024, 768))
+        .build(&event_loop)?;
+
+    let mut app = unsafe { App::create(&window) }?;
+    let mut destroying = false;
+    event_loop.run(move |event, _, control_flow| {});
+
+    Ok(())
+}
+
+#[derive(Clone, Debug)]
+struct App {}
+
+impl App {
+    unsafe fn create(window: &Window) -> Result<Self> {
+        Ok(Self {})
+    }
+
+    unsafe fn render(&mut self, window: &Window) -> Result<()> {
+        Ok(())
+    }
+
+    unsafe fn destroy(&mut self) {}
+}
+
+#[derive(Clone, Debug, Default)]
+struct AppData {}
