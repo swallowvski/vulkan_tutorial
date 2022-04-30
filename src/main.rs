3307b831fe96cc6922fa3a2d67efcb849cec5a0a
swallowvski <ahoudori7779@icloud.com>
2022-05-01 00:13:13 +0900

    add graphics queue

--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,7 @@ struct App {
     entry: Entry,
     instance: Instance,
     data: AppData,
+    device: Device,
 }
 
 impl App {
@@ -73,11 +74,13 @@ impl App {
         let mut data = AppData::default();
         let instance = create_instance(window, &entry, &mut data)?;
         pick_physical_device(&instance, &mut data)?;
+        let device = create_logical_device(&instance, &mut data)?;
 
         Ok(Self {
             entry,
             instance,
             data,
+            device,
         })
     }
 
@@ -86,6 +89,7 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) {
+        self.device.destroy_device(None);
         self.instance
             .destroy_debug_utils_messenger_ext(self.data.messenger, None);
         self.instance.destroy_instance(None);
@@ -96,6 +100,7 @@ impl App {
 struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
@@ -204,6 +209,31 @@ unsafe fn check_physical_device(
 
     QueueFamilyIndices::get(instance, data, physical_device)?;
     Ok(())
+}
+
+unsafe fn create_logical_device(instance: &Instance, data: &mut AppData) -> Result<Device> {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let queue_priorities = &[1.];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    let layers = vec![VALIDATION_LAYER.as_ptr()];
+
+    let features = vk::PhysicalDeviceFeatures::builder();
+
+    let queue_infos = &[queue_info];
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
 }
 
 #[derive(Copy, Clone, Debug)]
