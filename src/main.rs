9eb017e428dfb45bc31b39d7fcdd145624bee35f
swallowvski <ahoudori7779@icloud.com>
2022-05-02 00:20:31 +0900

    create pipeline

--- src/main.rs
+++ src/main.rs
@@ -96,6 +96,7 @@ impl App {
     }
 
     unsafe fn destroy(&mut self) {
+        self.device.destroy_pipeline(self.data.pipeline, None);
         self.device
             .destroy_pipeline_layout(self.data.pipeline_layout, None);
         self.device.destroy_render_pass(self.data.render_pass, None);
@@ -126,6 +127,7 @@ struct AppData {
     swapchain_image_views: Vec<vk::ImageView>,
     render_pass: vk::RenderPass,
     pipeline_layout: vk::PipelineLayout,
+    pipeline: vk::Pipeline,
 }
 
 unsafe fn create_instance(window: &Window, entry: &Entry, data: &mut AppData) -> Result<Instance> {
@@ -545,6 +547,24 @@ unsafe fn create_pipeline(device: &Device, data: &mut AppData) -> Result<()> {
 
     data.pipeline_layout = device.create_pipeline_layout(&layout_info, None)?;
 
+    let stages = &[vert_stage, frag_stage];
+    let info = vk::GraphicsPipelineCreateInfo::builder()
+        .stages(stages)
+        .vertex_input_state(&vertex_input_state)
+        .input_assembly_state(&input_assembly_state)
+        .viewport_state(&viewport_state)
+        .rasterization_state(&rasterization_state)
+        .multisample_state(&multisample_state)
+        .color_blend_state(&color_blend_state)
+        .layout(data.pipeline_layout)
+        .render_pass(data.render_pass)
+        .subpass(0)
+        .base_pipeline_handle(vk::Pipeline::null())
+        .base_pipeline_index(-1);
+
+    data.pipeline = device
+        .create_graphics_pipelines(vk::PipelineCache::null(), &[info], None)?
+        .0;
     device.destroy_shader_module(vert_shader_module, None);
     device.destroy_shader_module(frag_shader_module, None);
     Ok(())
