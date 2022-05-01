0cb42e4aacf44013aa7a995f95018654a23724b0
swallowvski <ahoudori7779@icloud.com>
2022-05-02 00:08:41 +0900

    add render pass

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,7 @@ impl App {
         let device = create_logical_device(&instance, &mut data)?;
         create_swapchain(window, &instance, &device, &mut data)?;
         create_swapchain_image_veiws(&device, &mut data)?;
+        create_render_pass(&instance, &device, &mut data)?;
         create_pipeline(&device, &mut data)?;
 
         Ok(Self {
@@ -97,6 +98,7 @@ impl App {
     unsafe fn destroy(&mut self) {
         self.device
             .destroy_pipeline_layout(self.data.pipeline_layout, None);
+        self.device.destroy_render_pass(self.data.render_pass, None);
         self.data
             .swapchain_image_views
             .iter()
@@ -122,6 +124,7 @@ struct AppData {
     swapchain: vk::SwapchainKHR,
     swapchain_images: Vec<vk::Image>,
     swapchain_image_views: Vec<vk::ImageView>,
+    render_pass: vk::RenderPass,
     pipeline_layout: vk::PipelineLayout,
 }
 
@@ -424,6 +427,41 @@ unsafe fn create_swapchain_image_veiws(device: &Device, data: &mut AppData) -> R
             device.create_image_view(&info, None)
         })
         .collect::<Result<Vec<_>, _>>()?;
+    Ok(())
+}
+
+unsafe fn create_render_pass(
+    instance: &Instance,
+    device: &Device,
+    data: &mut AppData,
+) -> Result<()> {
+    let color_attachment = vk::AttachmentDescription::builder()
+        .format(data.swapchain_format)
+        .samples(vk::SampleCountFlags::_1)
+        .load_op(vk::AttachmentLoadOp::CLEAR)
+        .store_op(vk::AttachmentStoreOp::STORE)
+        .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
+        .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
+        .initial_layout(vk::ImageLayout::UNDEFINED)
+        .final_layout(vk::ImageLayout::PRESENT_SRC_KHR);
+
+    let color_attachment_ref = vk::AttachmentReference::builder()
+        .attachment(0)
+        .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
+
+    let color_attachments = &[color_attachment_ref];
+    let subpass = vk::SubpassDescription::builder()
+        .pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS)
+        .color_attachments(color_attachments);
+
+    let attachments = &[color_attachment];
+    let subpasses = &[subpass];
+    let info = vk::RenderPassCreateInfo::builder()
+        .attachments(attachments)
+        .subpasses(subpasses);
+
+    data.render_pass = device.create_render_pass(&info, None)?;
+
     Ok(())
 }
 
