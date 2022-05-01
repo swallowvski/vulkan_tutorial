1b143c880b75c3fe1958615a726c33afae32842a
swallowvski <ahoudori7779@icloud.com>
2022-05-01 22:47:57 +0900

    add shader module

--- src/main.rs
+++ src/main.rs
@@ -80,6 +80,7 @@ impl App {
         let device = create_logical_device(&instance, &mut data)?;
         create_swapchain(window, &instance, &device, &mut data)?;
         create_swapchain_image_veiws(&device, &mut data)?;
+        create_pipeline(&device, &mut data)?;
 
         Ok(Self {
             entry,
@@ -421,6 +422,42 @@ unsafe fn create_swapchain_image_veiws(device: &Device, data: &mut AppData) -> R
         })
         .collect::<Result<Vec<_>, _>>()?;
     Ok(())
+}
+
+unsafe fn create_pipeline(device: &Device, data: &mut AppData) -> Result<()> {
+    let vert = include_bytes!("../shaders/vert.spv");
+    let frag = include_bytes!("../shaders/frag.spv");
+
+    let vert_shader_module = create_shader_module(device, &vert[..])?;
+    let frag_shader_module = create_shader_module(device, &frag[..])?;
+
+    let vert_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::VERTEX)
+        .module(vert_shader_module)
+        .name(b"main\0");
+
+    let frag_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::FRAGMENT)
+        .module(frag_shader_module)
+        .name(b"main\0");
+
+    device.destroy_shader_module(vert_shader_module, None);
+    device.destroy_shader_module(frag_shader_module, None);
+    Ok(())
+}
+
+unsafe fn create_shader_module(device: &Device, bytecode: &[u8]) -> Result<vk::ShaderModule> {
+    let bytecode = Vec::<u8>::from(bytecode);
+    let (prefix, code, suffix) = bytecode.align_to::<u32>();
+    if !prefix.is_empty() || !suffix.is_empty() {
+        return Err(anyhow!("SShader bytecode is not properly aligned."));
+    }
+
+    let info = vk::ShaderModuleCreateInfo::builder()
+        .code_size(bytecode.len())
+        .code(code);
+
+    Ok(device.create_shader_module(&info, None)?)
 }
 
 #[derive(Copy, Clone, Debug)]
